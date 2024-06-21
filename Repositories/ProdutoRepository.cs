48ee5ad22b7b04ec24a01edb11e0080b8ee46a40
Diogo215514 <marcosdiogovj@gmail.com>
2024-06-21 10:39:23 -0300

    Repository criado e model arrumada

---
+++ Repositories/ProdutoRepository.cs
@@ -0,0 +1,30 @@
+﻿using CardapioWeb.Context;
+using CardapioWeb.Models;
+
+namespace CardapioWeb.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDBContext _dbContext;
+
+        public ProdutoRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Produto> GetAll()
+        {
+            return _dbContext.Produtos.ToList();
+        }
+
+        public Produto GetById(int id)
+        {
+            return _dbContext.Produtos.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Produto> GetByPreferido()
+        {
+            return _dbContext.Produtos.Where(p => p.ProdutoPreferido).ToList();
+        }
+    }
+}
